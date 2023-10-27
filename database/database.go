e7eb7486268465078c294e086d5d8e3a9f9a36e7
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-10-27 15:02:12 +0100

    env error fixed

--- database/database.go
+++ database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -12,7 +13,7 @@ var DBClient *sqlx.DB
 func InitDB() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		panic(err)
+		fmt.Print(err)
 	}
 	db, err := sqlx.Connect("postgres", os.Getenv("DB_KEY"))
 	if err != nil {
